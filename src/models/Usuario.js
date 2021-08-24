ea132e8438827651f1bb66e6640947b803e99833
Practicante Operaciones y Tecnologias <practicanteot@clubint.com>
2021-08-24 13:54:56 -0500

    conexion fb

--- src/models/Usuario.js
+++ src/models/Usuario.js
@@ -2,6 +2,12 @@ const sql = require('mssql');
 /**
  * 
  * const [user,setUser] = React.useState([]);
+ * const [consulta,setConsulta] =React.useState(null);
+ * 
+ * const enviarConsulta = async (objeto) => {
+ * const res = await axios.post('http://localhost:4000/api/users');
+ * setConsulta(get.target.value)
+ * }
  * 
  * 
  * async componentDidMOunt{
